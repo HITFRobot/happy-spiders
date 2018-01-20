61d4b158e50329151ab0596801dfcaa33ead428a
conghuaicai <neuclil8107@gmail.com>
2018-01-20 22:08:47 +0800

    update sql

--- lagou/data/mysql_job_table.sql
+++ lagou/data/mysql_job_table.sql
@@ -1,18 +1,22 @@
-create table articles
+create table job
 (
-	title varchar(200) not null,
-	create_date date null,
 	url varchar(300) not null,
 	url_object_id varchar(50) not null
 		primary key,
-	front_image_url varchar(300) null,
-	front_image_path varchar(200) null,
-	praise_nums int null,
-	fav_nums int null,
-	comment_nums int null,
-	content longtext null,
-	tags varchar(200) null
+	title varchar(100) not null,
+	salary varchar(20) null,
+	job_city varchar(10) null,
+	work_years varchar(100) null,
+	degree_need varchar(30) null,
+	job_type varchar(20) null,
+	publish_time varchar(20) not null,
+	tags varchar(100) null,
+	job_advantage varchar(1000) null,
+	job_desc longtext not null,
+	job_addr varchar(50) null,
+	company_url varchar(300) null,
+	company_name varchar(100) null,
+	crawl_time datetime null
 )
 ;
-
 
